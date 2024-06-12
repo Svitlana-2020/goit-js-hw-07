bdc1bb25be26a29bcdef665c33f529e5cbbce2a9
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2024-06-12 14:05:28 +0100

    task 1 fault

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+const CategoriesItem = document.querySelectorAll(".item");
+const CategoriesArray = [...CategoriesItem];
+const NumCategories = CategoriesArray.length;
+console.log(NumCategories);
+
+// const categories = document.querySelector("#categories");
+// console.log(categories.children);
